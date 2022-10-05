95d736be35ad6d5ea884ce399b4511f36ae84367
Yifan Lin <yflin001217@gmail.com>
2022-10-06 00:38:05 +0800

    add login and signup buttons' binding functions rename "sign in" to "sign up" remove pingpong code

--- app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
+
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -15,62 +20,84 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.mysecondapp.MESSAGE";
+    private EditText userID, userPwd;
+    private final OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT > 9){
-            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
-        }
+        setContentView(R.layout.login_screen);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+        userID = (EditText) findViewById(R.id.text_userid);
+        userPwd = (EditText) findViewById(R.id.text_userpwd);
+
     }
 
-    public void hello(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        String message = "connection failed";
-
-        HttpURLConnection conn = null;
-        String url = "http://47.93.251.137:3000/";
+    public void login(View view) {
+        String id = userID.getText().toString().trim();
+        String pwd = userPwd.getText().toString().trim();
         try {
-            URL mURL = new URL(url);
-            conn = (HttpURLConnection) mURL.openConnection();
-
-            conn.setRequestMethod("GET");
-            conn.setReadTimeout(5000);
-            conn.setConnectTimeout(10000);
-
-            int responseCode = conn.getResponseCode();
-            if(responseCode == 200){
-                InputStream is = conn.getInputStream();
-                message = getStringFromInputStream(is);
+            String jsonStr = HttpLoginSignup(id, pwd, "login");
+            try {
+                JSONObject json = new JSONObject(jsonStr);
+                long code = json.getLong("code");
+                if (code == 1)
+                    System.out.println("成功");
+                else
+                    System.out.println("登录失败, 用户名或密码错误!");
             }
-            else{
-                message = "fuck";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if(conn != null){
-                conn.disconnect();
+            catch (JSONException ex) {
+                ex.printStackTrace();
             }
         }
-
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private static String getStringFromInputStream(InputStream is) throws IOException{
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int len = -1;
-        while((len = is.read(buffer)) != -1){
-            os.write(buffer, 0, len);
+    public void signup(View view) {
+        String id = userID.getText().toString().trim();
+        String pwd = userPwd.getText().toString().trim();
+        try {
+            String jsonStr = HttpLoginSignup(id, pwd, "register");
+            try {
+                JSONObject json = new JSONObject(jsonStr);
+                long code = json.getLong("code");
+                if (code == 1)
+                    System.out.println("成功!");
+                else
+                    System.out.println("注册失败, 用户名已被注册!");
+            }
+            catch (JSONException ex) {
+                ex.printStackTrace();
+            }
         }
-        is.close();
-        String state = os.toString();
-        os.close();
-        System.out.println(state);
-        return state;
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String HttpLoginSignup(String id, String pwd, String port) throws IOException{
+        HttpUrl.Builder urlBuilder
+                = HttpUrl.parse("http://47.93.251.137:3000/" + port).newBuilder();
+        urlBuilder.addQueryParameter("username", id);
+        urlBuilder.addQueryParameter("password", pwd);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        if (response.code() != 200)
+            throw new IOException();
+        return response.body().string();
     }
 }
