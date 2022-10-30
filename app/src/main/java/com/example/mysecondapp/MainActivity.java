1cd926371d06ed13dec68cf431d8c50ab2c009a8
Yifan Lin <yflin001217@gmail.com>
2022-10-30 15:40:22 +0800

    aligning login screen, asynchronize okhttp call

--- app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -2,25 +2,19 @@ package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DownloadManager;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 import org.json.JSONException;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -28,7 +22,7 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.mysecondapp.MESSAGE";
-    private EditText userID, userPwd;
+    private EditText etUserID, etUserPwd;
     private final OkHttpClient client = new OkHttpClient();
 
     @Override
@@ -36,68 +30,89 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
-        userID = (EditText) findViewById(R.id.text_userid);
-        userPwd = (EditText) findViewById(R.id.text_userpwd);
-
+        etUserID = findViewById(R.id.etUserID);
+        etUserPwd = findViewById(R.id.etUserPwd);
     }
 
     public void login(View view) {
-        String id = userID.getText().toString().trim();
-        String pwd = userPwd.getText().toString().trim();
-        try {
-            String jsonStr = HttpLoginSignup(id, pwd, "login");
-            try {
-                JSONObject json = new JSONObject(jsonStr);
-                long code = json.getLong("code");
-                if (code == 1)
-                    System.out.println("成功");
-                else
-                    System.out.println("登录失败, 用户名或密码错误!");
-            }
-            catch (JSONException ex) {
-                ex.printStackTrace();
-            }
-        }
-        catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void signup(View view) {
-        String id = userID.getText().toString().trim();
-        String pwd = userPwd.getText().toString().trim();
-        try {
-            String jsonStr = HttpLoginSignup(id, pwd, "register");
-            try {
-                JSONObject json = new JSONObject(jsonStr);
-                long code = json.getLong("code");
-                if (code == 1)
-                    System.out.println("成功!");
-                else
-                    System.out.println("注册失败, 用户名已被注册!");
-            }
-            catch (JSONException ex) {
-                ex.printStackTrace();
-            }
-        }
-        catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public String HttpLoginSignup(String id, String pwd, String port) throws IOException{
+        String id = etUserID.getText().toString().trim();
+        String pwd = etUserPwd.getText().toString().trim();
         HttpUrl.Builder urlBuilder
-                = HttpUrl.parse("http://47.93.251.137:3000/" + port).newBuilder();
+                = HttpUrl.parse("http://47.93.251.137:3000/" + "login").newBuilder();
         urlBuilder.addQueryParameter("username", id);
         urlBuilder.addQueryParameter("password", pwd);
         String url = urlBuilder.build().toString();
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        Call call = client.newCall(request);
-        Response response = call.execute();
-        if (response.code() != 200)
-            throw new IOException();
-        return response.body().string();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "网络连接出错!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String jsonStr = response.body().string();
+                    try {
+                        JSONObject json = new JSONObject(jsonStr);
+                        long retCode = json.getLong("code");
+                        if (retCode == 1) {
+                            Toast.makeText(MainActivity.this, "登录成功!", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "登录失败! 账号或密码错误.", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public void signup(View view) {
+        String id = etUserID.getText().toString().trim();
+        String pwd = etUserPwd.getText().toString().trim();
+        HttpUrl.Builder urlBuilder
+                = HttpUrl.parse("http://47.93.251.137:3000/" + "signup").newBuilder();
+        urlBuilder.addQueryParameter("username", id);
+        urlBuilder.addQueryParameter("password", pwd);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "网络连接出错!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String jsonStr = response.body().string();
+                    try {
+                        JSONObject json = new JSONObject(jsonStr);
+                        long retCode = json.getLong("code");
+                        if (retCode == 1) {
+                            Toast.makeText(MainActivity.this, "注册成功!", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "注册失败! 用户名已存在.", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
