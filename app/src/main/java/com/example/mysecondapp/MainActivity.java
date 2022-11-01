b9e4cfe65da4e7c175c71b26726fb36da323a09a
Yifan Lin <yflin001217@gmail.com>
2022-11-01 11:18:47 +0800

    login button demo

--- app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, LoginUtils.class);
-        startActivity(intent);
+        if (LoginUtils.checkForLogin(this, () -> onClick(view)))
+            Toast.makeText(this, "已登录", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent data) {
+        super.onActivityResult(reqCode, resCode, data);
+        if (data == null || resCode != Activity.RESULT_OK)
+            return;
+        String loginData = data.getStringExtra("key_login");
+        if (!loginData.equals("from_login"))
+            return;
+        LoginUtils.loginSuccess(reqCode);
     }
 }
