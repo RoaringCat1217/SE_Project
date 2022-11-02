b023154ec45e00385d61ddcd534840a80a8ceae2
Yifan Lin <yflin001217@gmail.com>
2022-11-02 23:18:54 +0800

    fix bugs for likes

--- app/src/main/java/com/example/mysecondapp/HotListEntry.java
+++ app/src/main/java/com/example/mysecondapp/HotListEntry.java
@@ -4,24 +4,30 @@ public class HotListEntry implements Comparable<HotListEntry>{
     private String group;
     private int hotIndex;
     private int rank;
+    private int usrPortrait;
+    private String usrID = "test";
+    private String title;
+    private String content = "test";
+    private int likes = -1;
 
-    public int usr_portrait;
-    public String usr_id;
-    public String post_title;
-    public String post_content;
-    public String liking_number;
+    public HotListEntry(String group, String title, int hotIndex, int rank) {
+        this.group = group;
+        this.title = title;
+        this.hotIndex = hotIndex;
+        this.rank = rank;
+    }
 
     public HotListEntry(String group, int hotIndex, int rank,
-                        int usr_portrait, String usr_id, String post_title, String post_content, String liking_number) {
+                        int usrPortrait, String usrID, String title, String content,
+                        int likes) {
         this.group = group;
         this.hotIndex = hotIndex;
         this.rank = rank;
-
-        this.usr_portrait = usr_portrait;
-        this.usr_id = usr_id;
-        this.post_title = post_title;
-        this.post_content = post_content;
-        this.liking_number = liking_number;
+        this.usrPortrait = usrPortrait;
+        this.usrID = usrID;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
     }
 
     @Override
@@ -30,22 +36,22 @@ public class HotListEntry implements Comparable<HotListEntry>{
     }
 
     public int getUsrPortrait() {
-        return usr_portrait;
+        return usrPortrait;
     }
 
     public String getUsrId() {
-        return usr_id;
+        return usrID;
     }
 
-    public String getPostTitle() {
-        return post_title;
+    public String getTitle() {
+        return title;
     }
 
-    public String getPostContent() {
-        return post_content;
+    public String getContent() {
+        return content;
     }
 
-    public String getLikingNumber() {
-        return liking_number;
+    public int getLikes() {
+        return likes;
     }
 }
