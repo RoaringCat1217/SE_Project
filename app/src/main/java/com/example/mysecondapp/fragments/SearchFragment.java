a2f63e57bc99ab8ae07543c18adecf239fd9dad2
Yifan Lin <yflin001217@gmail.com>
2022-12-07 22:36:03 +0800

    增加后端POST接口

--- app/src/main/java/com/example/mysecondapp/fragments/SearchFragment.java
+++ app/src/main/java/com/example/mysecondapp/fragments/SearchFragment.java
@@ -15,6 +15,7 @@ import com.example.mysecondapp.R;
 import com.example.mysecondapp.adapters.PostListAdapter;
 import com.example.mysecondapp.models.EntryPost;
 import com.example.mysecondapp.utils.BackendUtils;
+import com.example.mysecondapp.utils.Constants;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,21 +34,33 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_hits_favorite_group, container, false);
-
         rv = (RecyclerView) view.findViewById(R.id.rv_list);
         searchListAdapter = new PostListAdapter(getActivity());
         rv.setAdapter(searchListAdapter);
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        Bundle bundle = getArguments();
+        assert bundle != null;
+        String query = bundle.getString(Constants.SEARCH_KEYS);
+
+        JSONArray keys = new JSONArray();
+        for (String key: query.split("\\s+"))
+            keys.put(key);
+        JSONObject json = new JSONObject();
+        try {
+            json.put("keywords", keys);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         searchListData = new ArrayList<>();
         searchListAdapter.setPostList(searchListData);
-        fetchSearchList();
+        fetchSearchList(json);
 
         return view;
     }
 
-    public void fetchSearchList() {
-        // BackendUtils.get(getActivity(), "hot", null, this::fetchHotListCallback);
+    public void fetchSearchList(JSONObject json) {
+        BackendUtils.post(getActivity(), "search", json, this::fetchSearchListCallback);
     }
 
     private void fetchSearchListCallback(JSONObject json) {
@@ -55,9 +68,19 @@ public class SearchFragment extends Fragment {
         try {
             long retCode = json.getLong("code");
             if (retCode == 1) {
-
+                JSONArray arr = json.getJSONArray("entry");
+                int length = arr.length();
+                for (int i = 0; i < length; i++) {
+                    JSONObject entry = arr.getJSONObject(i);
+                    String title = entry.getString("title");
+                    String content = entry.getString("content");
+                    String group = entry.getString("group_name");
+                    int hotIndex = length - i;
+                    int postID = entry.getInt("post_id");
+                    searchListData.add(new EntryPost(postID, title, hotIndex, group, content));
+                }
             } else {
-
+                Toast.makeText(getActivity(), "搜索失败! 请刷新再试.", Toast.LENGTH_SHORT).show();
             }
         } catch (JSONException e) {
             e.printStackTrace();
--- app/src/main/java/com/example/mysecondapp/utils/BackendUtils.java
+++ app/src/main/java/com/example/mysecondapp/utils/BackendUtils.java
@@ -1,6 +1,7 @@
 package com.example.mysecondapp.utils;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.os.Looper;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ public class BackendUtils {
             .retryOnConnectionFailure(true)
             .build();
     private static final String baseURL = "http://47.93.251.137:3000/";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public static void get(Activity activity, String path, Map<String, String> query, BackendCallback callback) {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(baseURL + path).newBuilder();
@@ -33,6 +35,42 @@ public class BackendUtils {
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("Connection", "close")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Looper.prepare();
+                Toast.makeText(activity, "网络连接出错!", Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Looper.prepare();
+                    Toast.makeText(activity, "服务器端出错!", Toast.LENGTH_SHORT).show();
+                    Looper.loop();
+                } else {
+                    String jsonStr = response.body().string();
+                    try {
+                        JSONObject json = new JSONObject(jsonStr);
+                        activity.runOnUiThread(()->{callback.run(json);});
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public static void post(Activity activity, String path, JSONObject json, BackendCallback callback) {
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(baseURL + path).newBuilder();
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
                 .build();
         client.newCall(request).enqueue(new Callback() {
 
